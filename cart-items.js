11db7842b879373a5162efc77b69f386fe048a4e
Spencer K-W <keashlywarters@gmail.com>
2019-05-31 13:45:23 -0400

    separated cart items into their own file

---
+++ cart-items.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const shoppingCart = [
+  {
+    id: 11,
+    product: "teddy bear",
+    price: 15,
+    quantity: 5
+  },
+  {
+    id: 12,
+    product: "book",
+    price: 10,
+    quantity: 15
+  },
+  {
+    id: 13,
+    product: "burger",
+    price: 8,
+    quantity: 3
+  },
+  {
+    id: 14,
+    product: "cookies",
+    price: 12,
+    quantity: 8
+  }
+];
+
+module.exports = shoppingCart;
--- routes.js
+++ routes.js
@@ -1,33 +1,7 @@
 "use strict";
 const express = require("express");
 const cart = express.Router();
-
-const shoppingCart = [
-  {
-    id: 11,
-    product: "teddy bear",
-    price: 15,
-    quantity: 5
-  },
-  {
-    id: 12,
-    product: "book",
-    price: 10,
-    quantity: 15
-  },
-  {
-    id: 13,
-    product: "burger",
-    price: 8,
-    quantity: 3
-  },
-  {
-    id: 14,
-    product: "cookies",
-    price: 12,
-    quantity: 8
-  }
-];
+const shoppingCart = require("./cart-items");
 
 cart.get("/cart-items", (req, res) => {
   res.json(shoppingCart);
