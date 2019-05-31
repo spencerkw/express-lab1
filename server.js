d98978bef7f9aa1a113deba9eecd9cd6a7198c92
Spencer K-W <keashlywarters@gmail.com>
2019-05-31 13:35:44 -0400

    updated server route

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ const port = 3000;
 
 app.use(express.static(__dirname + "/public"));
 app.use(express.json());
-app.use("/", routes);
+app.use("/api", routes);
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
