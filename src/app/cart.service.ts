946c03eabe976edb1e146cbaf320ad8cbf6e0c92
Spencer K-W <keashlywarters@gmail.com>
2019-05-31 15:04:07 -0400

    added the other 3 methods

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { CartItem } from './cart-item';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +11,18 @@ export class CartService {
   constructor(private http: HttpClient) { }
 
   getAllItems(): Observable<any> {
-    return this.http.get("/api/cart-items", {responseType: "json"});
+    return this.http.get("/api/cart-items", { responseType: "json" });
+  }
+
+  addItem(newItem: CartItem): Observable<any> {
+    return this.http.post("/api/cart-items", newItem, { responseType: "json" });
+  }
+
+  updateItem(id: number, newItem: CartItem): Observable<any> {
+    return this.http.put(`/api/cart-items/${id}`, newItem, { responseType: "json" });
+  }
+
+  deleteItem(id: number): Observable<any> {
+    return this.http.delete(`/api/cart-items/${id}`, { responseType: "json" });
   }
 }
