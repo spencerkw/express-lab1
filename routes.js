4b00cf805a72b2dc8e059d5277001b5f514e5e50
Spencer K-W <keashlywarters@gmail.com>
2019-06-04 15:26:44 -0400

    backend uses DB now

--- routes.js
+++ routes.js
@@ -1,31 +1,44 @@
 "use strict";
 const express = require("express");
 const cart = express.Router();
+const pool = require("./connection");
 const shoppingCart = require("./cart-items");
 
+function selectAll(res) {
+  pool.query("SELECT * FROM shoppingcart ORDER BY id").then(result => {
+    res.json(result.rows);
+  })
+}
+
 cart.get("/cart-items", (req, res) => {
-  res.json(shoppingCart);
+  selectAll(res);
 });
 
 cart.post("/cart-items", (req, res) => {
-  // console.log(req.body);
-  shoppingCart.push(req.body);
-  res.json(shoppingCart);
+  pool.query("INSERT INTO shoppingcart (product, price, quantity) VALUES ($1::text, $2::real, $3::int)", [
+    req.body.product,
+    req.body.price,
+    req.body.quantity
+  ]).then(() => {
+    selectAll(res);
+  });
 });
 
 cart.put("/cart-items/:id", (req, res) => {
-  // console.log(req.params.id);
-  // console.log(req.body);
-  let index = shoppingCart.findIndex(item => item.id == req.params.id);
-  shoppingCart.splice(index, 1, req.body);
-  res.json(shoppingCart);
+  pool.query("UPDATE shoppingcart SET product=$1::text, price=$2::real, quantity=$3::int WHERE id=$4::int", [
+    req.body.product,
+    req.body.price,
+    req.body.quantity,
+    req.params.id
+  ]).then(() => {
+    selectAll(res);
+  });
 });
 
 cart.delete("/cart-items/:id", (req, res) => {
-  // console.log(req.params.id);
-  let index = shoppingCart.findIndex(item => item.id == req.params.id);
-  shoppingCart.splice(index, 1);
-  res.json(shoppingCart);
+  pool.query("DELETE FROM shoppingcart WHERE id=$1::int", [Number(req.params.id)]).then(() => {
+    selectAll(res);
+  });
 });
 
 module.exports = cart;
